2b3601c6885590af891df87df4037be343e704ee
megha55 <megha55@hotmail.com>
2012-10-28 15:17:19 +0530

    Javascript functions

--- javascript.cc
+++ javascript.cc
@@ -32,18 +32,28 @@ void JS :: addRollNoFields()
         << "for(j=0; j<5; j++)" << endl
         << "{" << endl
         << "var cell=row.insertCell( j );" << endl
-        << "var input = document.createElement(\"input\");" << endl
-        << "input.type = \"text\";" << endl
-        << "input.defaultValue = FieldName[j] + " " + (i + 1);" << endl
-        << "input.name = FieldName[j] + \"_\" + (i + 1);" << endl
-//        << "var select = document.createElement(\"select\");" << endl
-//        << "var option = document.createElement(\"option\");" << endl
-//        << "option.text = FieldName[j] + \" \" + (i + 1);" << endl
-//        << "option.value = FieldName[j] + (i + 1);" << endl
-//        << "select.name = FieldName[j] + \"_\" + (i + 1); " << endl 
-//        << "select.appendChild(option);" << endl
-//        << "cell.appendChild(select);" << endl
-        << "cell.appendChild(input);" << endl
+//        << "var input = document.createElement(\"input\");" << endl
+//        << "input.type = \"text\";" << endl
+//        << "input.defaultValue = FieldName[j] + " " + (i + 1);" << endl
+//        << "input.name = FieldName[j] + \"_\" + (i + 1);" << endl
+        << "var select = document.createElement(\"select\");" << endl
+        << "var option = document.createElement(\"option\");" << endl
+        << "option.text = FieldName[j] + \" \" + (i + 1);" << endl
+        << "option.value = FieldName[j] + (i + 1);" << endl
+        << "select.name = FieldName[j] + \"_\" + (i + 1); " << endl 
+        << "select.appendChild(option);" << endl
+        << "cell.appendChild(select);" << endl
+        << "if(j == 0)" << endl
+        << "{" <<endl
+        << "for(k = 0; k < " << BranchSize << "; k++ )" << endl
+        << "{" << endl
+        << "select.options[select.options.length] = new Option(BranchNames[k], BranchNames[k]); " //<< endl
+//        sel.options[sel.options.length] = new Option("text3","value3"); 
+        << "var option = document.createElement(\"option\");" << endl
+        << "select.appendChild(option);" //<< endl
+        << "}" << endl
+        << "}" << endl
+//        << "cell.appendChild(input);" << endl
         << "}" << endl
         << "}" << endl
         << "}" << endl;
@@ -67,19 +77,19 @@ void JS :: addRoomFields()
         << "var row = table.insertRow(i);" << endl
         << "for(j=0; j<3; j++)" << endl
         << "{" << endl
-        << "var cell=row.insertCell( j );" << endl;
-//         << "if( j == 0)" << endl
-//         << "{" << endl
-//         << "var select = document.createElement(\"select\");" << endl
-//         << "var option = document.createElement(\"option\");" << endl
-//         << "option.text = FieldName[j] + " " + (i + 1);" << endl
-//         << "option.value = FieldName[j] + (i + 1);" << endl                    
-//         << "select.name = FieldName[j] + \"_\" + (i + 1);" << endl
-//         << "select.appendChild(option);" << endl
-//         << "cell.appendChild(select);" << endl
-//         << "}" << endl
-//         << "else" << endl
-   cout << "{" << endl
+        << "var cell=row.insertCell( j );" << endl
+        << "if( j == 0)" << endl
+        << "{" << endl
+        << "var select = document.createElement(\"select\");" << endl
+        << "var option = document.createElement(\"option\");" << endl
+        << "option.text = FieldName[j] + " " + (i + 1);" << endl
+        << "option.value = FieldName[j] + (i + 1);" << endl                    
+        << "select.name = FieldName[j] + \"_\" + (i + 1);" << endl
+        << "select.appendChild(option);" << endl
+        << "cell.appendChild(select);" << endl
+        << "}" << endl
+        << "else" << endl
+        << "{" << endl
         << "var input = document.createElement(\"input\"); " << endl
         << "input.type = \"text\";" << endl
         << "input.defaultValue = FieldName[j] + " " + (i + 1);" << endl
